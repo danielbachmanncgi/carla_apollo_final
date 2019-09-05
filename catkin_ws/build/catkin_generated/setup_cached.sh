662fbd026813e956284874e59e55e8b09d4b399c
Lukas <thede@fzi.de>
2019-09-05 12:47:45 +0200

    include player_vehicle node and apollo_control_to_calra node

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,5 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/carla/carla-apollo/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/carla/carla-apollo/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/carla/carla-apollo/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/carla/carla-apollo/catkin_ws/build"
-export ROS_PACKAGE_PATH="/home/carla/carla-apollo/nemodrive/catkin_ws/src:/home/carla/carla-apollo/catkin_ws/src:/opt/ros/melodic/share"
+export PYTHONPATH="/home/carla/carla-apollo/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/melodic/lib/python2.7/dist-packages:/home/carla/CARLA_0.9.5/PythonAPI/carla/:/home/carla/CARLA_0.9.5/PythonAPI/carla/agents/:/home/carla/scenario_runner-0.9.5:/home/carla/CARLA_0.9.5/PythonAPI/carla/dist/carla-0.9.5-py2.7-linux-x86_64.egg:/home/carla/CARLA_0.9.5/PythonAPI/carla/agents:/home/CARLA_0.9.5/PythonAPI/examples"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/carla/carla-apollo/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/carla/carla-apollo/nemodrive/catkin_ws/src:/home/carla/carla-apollo/catkin_ws/src:$ROS_PACKAGE_PATH"
